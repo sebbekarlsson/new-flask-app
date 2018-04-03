ce7fdfb7a202f5f744d236b1cdf24274c0c58230
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-03 13:23:21 +0200

    added some output

--- new_flask_app/bin.py
+++ new_flask_app/bin.py
@@ -8,6 +8,8 @@ CWD = os.getcwd()
 
 
 def run():
+    print('Creating your flask application...')
     shutil.copytree(
         '{}/.new-flask-app/boilerplate'.format(HOME), CWD + '/' + 'app'
     )
+    print('The application was created!')
